580ac6eef0890fcb3a743e1b0b71140f1c46b536
Justus Bruegmann <65978828+justusbruegmann@users.noreply.github.com>
2026-02-05 15:29:43 +0100

    Array aufgabe hinzugefügt

---
+++ src/de/dhbwka/java/exercise/math/Array.java
@@ -0,0 +1,26 @@
+public class Array {
+    public static void main() {
+        int size = 10;
+        double[] array = new double[size];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Math.random()*100;
+        }
+        for (double v : array) {
+            System.out.println(v);
+        }
+        double sum = 0;
+        double max = 0;
+        int maxIndex = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                maxIndex = i;
+                max = array[i];
+            }
+            sum += array[i];
+        }
+        System.out.println("Maximums index "+maxIndex);
+        System.out.println("maximum "+max);
+        double avg = (double) 1 /size;
+        System.out.println("Average "+(avg*sum));
+    }
+}
