5659f3d60d2499c85757c4dabfdc402a18d701aa
sebbi <sebastian@anhalt-net.de>
2026-03-12 13:59:10 +0100

    Remove duplicated exercise

--- src/de/dhbwka/java/course/exceptions/DivZeroException.java
+++
@@ -1,9 +0,0 @@
-package de.dhbwka.java.course.exceptions;
-
-public class DivZeroException extends Exception {
-    public DivZeroException(){
-    }
-    public DivZeroException(String message){
-        super(message);
-    }
-}
