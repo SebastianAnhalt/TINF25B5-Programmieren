66e70fe9944984d46df8629699a51ec279c3b6f9
Anis Koubaa <mohamed.koubaa@kit.edu>
2026-05-27 09:01:35 +0200

    Add InterfaceA Example.

---
+++ src/de/dhbwka/java/course/session_04/ClassA.java
@@ -0,0 +1,8 @@
+package de.dhbwka.java.course.session_04;
+
+public class ClassA implements InterfaceA {
+    public static void main(String[] args) {
+        ClassA cl = new ClassA();
+        cl.methodA();
+    }
+}
