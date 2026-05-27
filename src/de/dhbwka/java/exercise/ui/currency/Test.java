49e2024547ae133d792bce74c000531a5faae642
justu <justusbruegmann@gmail.com>
2026-05-27 10:16:01 +0200

    added server

---
+++ src/de/dhbwka/java/exercise/ui/currency/Test.java
@@ -0,0 +1,8 @@
+package de.dhbwka.java.exercise.ui.currency;
+
+public class Test {
+    static void main() {
+        FromServer service = new FromServer();
+        System.out.println(service.eur());
+    }
+}
